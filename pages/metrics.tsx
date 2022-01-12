00a666e39e333a35b538b8e6783a858a6c44bf1c
Pedro <pedrokaz.henrique@gmail.com>
2022-01-12 18:29:25 -0300

    feat: page only visible by ssr specific permissions

---
+++ pages/metrics.tsx
@@ -0,0 +1,28 @@
+import { setUpAPIClient } from "../services/api";
+
+import { withSSRAuth } from "../utils/withSSRAuth";
+
+export default function Metrics() {
+  return (
+    <>
+      <h1> Metrics </h1>
+    </>
+  );
+}
+
+export const getServerSideProps = withSSRAuth(
+  async (ctx) => {
+    const apiClient = setUpAPIClient(ctx);
+
+    const response = await apiClient.get("/me");
+    //console.log(response);
+
+    return {
+      props: {},
+    };
+  },
+  {
+    permissions: ["metrics.list"],
+    roles: ["administrator"],
+  }
+);
