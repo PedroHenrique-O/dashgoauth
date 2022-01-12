a3c3bbc3bd36fd2272686a35e95e75ad2342cb5b
Pedro <pedrokaz.henrique@gmail.com>
2022-01-11 21:19:26 -0300

    added SSRAuth handler along with serverSideProps

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,13 +1,28 @@
 import { useContext, useEffect } from "react";
-import { api } from "../../services/api";
+import { setUpAPIClient } from "../../services/api";
+import { api } from "../../services/apiClient";
+
 import { AuthContext } from "../../src/context/AuthContext";
+import { withSSRAuth } from "../../utils/withSSRAuth";
 
 export default function DashBoar() {
   const { user } = useContext(AuthContext);
 
   useEffect(() => {
-    api.get("/me").then((response) => console.log(response));
+    api
+      .get("/me")
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
   }, []);
 
   return <h1> Dashboard: {user?.email} </h1>;
 }
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+  const apiClient = setUpAPIClient(ctx);
+  const response = await apiClient.get("/me");
+  console.log(response.data);
+  return {
+    props: {},
+  };
+});
