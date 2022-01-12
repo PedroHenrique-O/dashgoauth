15b5cfeb87912cf83a02a8300a0af6ef7a328ec2
Pedro <pedrokaz.henrique@gmail.com>
2022-01-12 18:25:01 -0300

    refactor: user permissions with ssr auth

--- utils/withSSRAuth.ts
+++ utils/withSSRAuth.ts
@@ -5,20 +5,55 @@ import {
 } from "next";
 import { destroyCookie, parseCookies } from "nookies";
 import { AuthTokenError } from "../errors/AuthTokenError";
+import decode from "jwt-decode";
+import { validateUserPermissions } from "./validateUserPermissions";
 
-export function withSSRAuth<P>(fn: GetServerSideProps<P>): GetServerSideProps {
+type WithSSRAuthOptions = {
+  permissions?: string[];
+  roles?: string[];
+};
+
+export function withSSRAuth<P>(
+  fn: GetServerSideProps<P>,
+  options?: WithSSRAuthOptions
+): GetServerSideProps {
   return async (
     ctx: GetServerSidePropsContext
   ): Promise<GetServerSidePropsResult<P>> => {
     const cookies = parseCookies(ctx);
 
-    if (!cookies["nextauth.token"]) {
+    const token = cookies["nextauth.token"];
+
+    if (!token) {
       return {
         redirect: {
           destination: "/",
           permanent: false,
         },
       };
+    }
+
+    if (options) {
+      const user = decode<{ permissions: string[]; roles: string[] }>(token);
+      console.log(user);
+
+      const { permissions, roles } = options;
+
+      const userhasValidPermissions = validateUserPermissions({
+        user,
+        permissions,
+        roles,
+      });
+
+      if (!userhasValidPermissions) {
+        return {
+          //notFound: true,
+          redirect: {
+            destination: "/dashboard",
+            permanent: false,
+          },
+        };
+      }
     }
 
     try {
